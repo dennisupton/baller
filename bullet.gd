0f0d0fba6ca4b2c2c241b3a0b8988ef3e9575514
Dennis Upton <108183460+dennisupton@users.noreply.github.com>
2026-01-19 17:22:10 +0000

    Fix Bullets

--- bullet.gd
+++ bullet.gd
@@ -9,8 +9,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	position.y += -50
-	if $RayCast2D.is_colliding() and $RayCast2D.get_collider().is_in_group("ball"):
-		$RayCast2D.get_collider().health -= 1
+	if colliding():
+		colliding().health -= 1
 		queue_free()
 	elif position.y < 0:
 		queue_free()
+
+
+func colliding():
+	for i in $Area2D.get_overlapping_bodies():
+		if i.is_in_group("ball"):
+			return i
+	return false
